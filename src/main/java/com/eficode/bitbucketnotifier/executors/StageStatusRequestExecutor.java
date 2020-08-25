6c60952996fc58ac3fcc39ebc0b7cef10a0f5c46
Pasi Saikkonen <pasi.saikkonen@eficode.com>
2020-08-25 11:00:01 +0300

    Block notifications triggered by non bitbucket materials

--- src/main/java/com/eficode/bitbucketnotifier/executors/StageStatusRequestExecutor.java
+++ src/main/java/com/eficode/bitbucketnotifier/executors/StageStatusRequestExecutor.java
@@ -24,6 +24,7 @@ import com.eficode.bitbucketnotifier.requests.StageStatusRequest;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
 import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
@@ -35,6 +36,7 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Map;
 
 public class StageStatusRequestExecutor implements RequestExecutor {
     private static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create();
@@ -62,33 +64,42 @@ public class StageStatusRequestExecutor implements RequestExecutor {
 
     protected void sendNotification() throws Exception {
         PluginSettings pluginSettings = pluginRequest.getPluginSettings();
+        Map material = this.request.pipeline.buildCause.get(0).material;
 
-        String revision = this.request.pipeline.buildCause.get(0).modifications.get(0).revision;
-        URL url = new URL(pluginSettings.getApiUrl() + "/rest/build-status/1.0/commits/" + revision);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        if (material.get("type").equals("git")) {
+            LinkedTreeMap gitConfig = (LinkedTreeMap) material.get("git-configuration");
+            String gitUrl = (String) gitConfig.get("url");
 
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Authorization", getAuthHeader(pluginSettings));
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setDoOutput(true);
+            if (gitUrl.contains(pluginSettings.getApiUrl())) {
+                String revision = this.request.pipeline.buildCause.get(0).modifications.get(0).revision;
+                URL url = new URL(pluginSettings.getApiUrl() + "/rest/build-status/1.0/commits/" + revision);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-        OutputStream os = connection.getOutputStream();
-        String buildUrl = pluginSettings.getGoServerUrl() + "/go/pipelines/value_stream_map/" + request.pipeline.name + "/" + request.pipeline.counter;
-        byte[] input = ("{\"state\": \"" + this.parseBuildState(request.pipeline.stage.state) + "\", " +
-                "\"key\": \"" + revision + "\", " +
-                "\"name\": \"" + request.pipeline.counter + "\", " +
-                "\"url\": \"" + buildUrl + "\"}").getBytes("utf-8");
-        os.write(input, 0, input.length);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Authorization", getAuthHeader(pluginSettings));
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Accept", "application/json");
+                connection.setDoOutput(true);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
-        StringBuilder response = new StringBuilder();
-        String responseLine;
-        while ((responseLine = br.readLine()) != null) {
-            response.append(responseLine.trim());
+                OutputStream os = connection.getOutputStream();
+                String buildUrl = pluginSettings.getGoServerUrl() + "/go/pipelines/value_stream_map/" + request.pipeline.name + "/" + request.pipeline.counter;
+                byte[] input = ("{\"state\": \"" + this.parseBuildState(request.pipeline.stage.state) + "\", " +
+                        "\"key\": \"" + revision + "\", " +
+                        "\"name\": \"" + request.pipeline.counter + "\", " +
+                        "\"url\": \"" + buildUrl + "\"}").getBytes("utf-8");
+                os.write(input, 0, input.length);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8"));
+                StringBuilder response = new StringBuilder();
+                String responseLine;
+
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+                connection.getInputStream().close();
+            }
         }
-        System.out.println(response.toString());
-        connection.getInputStream().close();
     }
 
     private String parseBuildState(String stageState) {
